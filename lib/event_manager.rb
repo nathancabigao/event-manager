7f6b65a063232885bfe21f5ae11f342135a5f677
nathancabigao <nathan0714@hotmail.com>
2023-01-20 15:13:04 -0500

    Complete iteration 0

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,12 @@
 # frozen-string-literal: true
 
 puts 'Event Manager Initialized!'
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index|
+  next if index.zero?
+
+  columns = line.split(',')
+  name = columns[2]
+  puts name
+end
